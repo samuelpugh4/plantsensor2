35436f15217234b7b7589918fb17783433ea4347
samuelpugh4 <44142805+samuelpugh4@users.noreply.github.com>
2021-03-09 16:39:31 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,37 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (gatorEnvironment.getMeasurement(measurementType.eCO2)))
-    if (gatorEnvironment.getMeasurement(measurementType.eCO2) >= 50) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (gatorSoil.moisture(AnalogPin.P0, GatorSoilType.Moisture, DigitalPin.P0) > 50) {
-        basic.showString("Soil Moisture OK")
-    } else {
-        basic.showString("Soil Moisture LOW")
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-    }
+    led.plotBarGraph(
+    gatorEnvironment.getMeasurement(measurementType.eCO2),
+    100
+    )
 })
 input.onButtonPressed(Button.B, function () {
+    let strip: neopixel.Strip = null
     basic.showString("" + (gatorEnvironment.getMeasurement(measurementType.degreesF)))
     if (gatorEnvironment.getMeasurement(measurementType.degreesF) < 60) {
-        basic.showString("TOO COLD")
         basic.showIcon(IconNames.Sad)
+        for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+            strip.clear()
+        }
     } else if (gatorEnvironment.getMeasurement(measurementType.degreesF) > 90) {
-        basic.showString("TOO HOT")
         basic.showIcon(IconNames.Sad)
+        for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            strip.clear()
+        }
     } else {
         basic.showIcon(IconNames.Happy)
+        for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
+            strip.clear()
+        }
+    }
+})
+basic.forever(function () {
+    if (gatorSoil.moisture(AnalogPin.P0, GatorSoilType.Moisture, DigitalPin.P0) > 50) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
